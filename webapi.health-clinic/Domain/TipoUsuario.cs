d26cd19f3283eb85f5900f096fb0004101610307
Gabriel Henrique Alves da Silva <gabriel.hads8@gmail.com>
2023-09-25 17:17:06 -0300

    25.09.2023

---
+++ webapi.health-clinic/Domain/TipoUsuario.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.health_clinic.Domain
+{
+    [Table(nameof(TipoUsuario))]
+    public class TipoUsuario
+    {
+        [Key]
+        public Guid IdTipoUsuario { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "VARCHAR(50)")]
+        [Required(ErrorMessage = "Título é obrigatório!")]
+        public string? Titulo { get; set; }
+    }
+}
