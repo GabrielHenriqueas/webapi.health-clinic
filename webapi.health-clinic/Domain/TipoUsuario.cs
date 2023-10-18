b08760c9c130dc6b22444fdb87e99782c646dd1e
Gabriel Henrique Alves da Silva <gabriel.hads8@gmail.com>
2023-10-18 14:46:56 -0300

    18.10.2023

--- webapi.health-clinic/Domain/TipoUsuario.cs
+++ webapi.health-clinic/Domain/TipoUsuario.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace webapi.health_clinic.Domain
@@ -14,3 +15,4 @@ namespace webapi.health_clinic.Domain
         public string? Titulo { get; set; }
     }
 }
+
